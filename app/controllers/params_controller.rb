f8b7cab1c3617ab5b763b7f302db841ec21e7343
James <james.ryan.mcguire@gmail.com>
2021-06-17 14:17:13 -0700

    Adds guess method controller ad route

--- app/controllers/params_controller.rb
+++ app/controllers/params_controller.rb
@@ -1,10 +1,23 @@
 class ParamsController < ApplicationController
   def display_name
     input_value = params["show_name"]
-    if input_value[0].downcase == "a"
+    if input_value.downcase[0] == "a"
       render json: "Hey #{input_value}, your name starts with the first letter of the alphabet!"
     else
       render json: {show_name: "Your name is #{input_value}."}
     end
+  end
+
+  def guess
+    winning_number = 32
+    input_guess = params["the_users_guess"].to_i
+    if input_guess > winning_number 
+      output_message = "Pick lower!"
+    elsif input_guess < winning_number
+      output_message = "Pick higher!"
+    else
+      output_message = "You win!"
+    end
+    render json: {message: output_message}
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/display_name", controller: "params", action: "display_name"
+
+  get "/guess", controller: "params", action: "guess"
 end
