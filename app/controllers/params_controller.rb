50a2a4dbb52b38cb1020357894b92eec30f63701
James <james.ryan.mcguire@gmail.com>
2021-06-17 14:03:33 -0700

    Updates display_name controller method

--- app/controllers/params_controller.rb
+++ app/controllers/params_controller.rb
@@ -1,6 +1,10 @@
 class ParamsController < ApplicationController
   def display_name
     input_value = params["show_name"]
-    render json: {show_name: "Your name is #{input_value}."}
+    if input_value[0].downcase == "a"
+      render json: "Hey #{input_value}, your name starts with the first letter of the alphabet!"
+    else
+      render json: {show_name: "Your name is #{input_value}."}
+    end
   end
 end
